deed64d56f4d5f7a77f89080fb9c999f56c59aa2
Erica Wright <ericaewright@gmail.com>
2015-05-13 15:59:55 +0000

    write to file using 'fast-csv'

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-request = require('request');
-cheerio = require('cheerio');
+var request = require('request');
+var cheerio = require('cheerio');
+var path = require('path');
+var fs = require('fs');
+var csv = require('fast-csv'); 
+
+var csvStream = csv.createWriteStream({headers: true}),
+    writableStream = fs.createWriteStream("images.csv");
+ 
+writableStream.on("finish", function(){
+  console.log("DONE!");
+});
 
 request('http://substack.net/images/', function(error, response, body){
   if(error) {
-    console.log("...")
+    console.log("error")
     return;
   }
   var $ = cheerio.load(body);
+   csvStream.pipe(writableStream);
     $('tr').each(function(i, element){
-      var url = 'http://substack.net/images' + $(this).find('a').attr('href')
-      // var permissions = $(this).nth-child(2)
-
-     console.log($(this).children(':nth-child(1)').text());
+      var url = 'http://substack.net/images' + $(this).find('a').attr('href');
+      var permissions = $(this).children(':nth-child(1)').text();
+      var type = path.extname(url);
+      var all_three = [permissions,url,type];
+     
+      csvStream.write(all_three);
+    
     });
-  // console.log(body);
+    csvStream.end();
+
+
 });
+
+
+
